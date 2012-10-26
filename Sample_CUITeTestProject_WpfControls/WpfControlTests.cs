10cb2fd683ae60118a58958769547392d1756bec
icnocop_cp <SND\icnocop_cp>
2012-10-25 22:54:03 -0700

    added AutomationIds to wpf ListView added two integration tests: WpfListView_GetRow_CanClickOnRow and WpfListView_GetRow_CanClickOnCell

--- Sample_CUITeTestProject_WpfControls/WpfControlTests.cs
+++ Sample_CUITeTestProject_WpfControls/WpfControlTests.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using CUITe.Controls.WpfControls;
 using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sample_CUITeTestProject_WpfControls.ObjectLibrary;
 
@@ -102,6 +103,25 @@ namespace Sample_CUITeTestProject_WpfControls
             CUITe_WpfRow row0 = mainWindow.dg1.Get<CUITe_WpfRow>("AutomationId=Row0");
             CUITe_WpfCell row0cell0 = row0.Get<CUITe_WpfCell>("AutomationId=cellRow0Col0");
             Assert.AreEqual("XML in Action", row0cell0.Value);
+        }
+
+        [TestMethod]
+        public void WpfListView_GetRow_CanClickOnRow()
+        {
+            CUITe_WpfTable listView = mainWindow.Get<CUITe_WpfTable>("AutomationId=lv1");
+            Assert.AreEqual(8, listView.RowCount);
+            CUITe_WpfControl<WpfControl> control = listView.Get<CUITe_WpfControl<WpfControl>>("AutomationId=Item0");
+            control.Click();
+        }
+
+        [TestMethod]
+        public void WpfListView_GetRow_CanClickOnCell()
+        {
+            CUITe_WpfTable listView = mainWindow.Get<CUITe_WpfTable>("AutomationId=lv1");
+            Assert.AreEqual(8, listView.RowCount);
+            CUITe_WpfControl<WpfControl> control = listView.Get<CUITe_WpfControl<WpfControl>>("AutomationId=Item0");
+            CUITe_WpfCell cell = control.Get<CUITe_WpfCell>("ColumnHeader=Content");
+            cell.Click();
         }
     }
 }
