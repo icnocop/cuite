03fab1a29ee04f4dd4156f9221cd7f0810f83dac
icnocop_cp <SND\icnocop_cp>
2013-01-23 23:32:07 -0800

    updated to version 1.6 and using CommonAssemblyInfo.cs fixed reference to Microsoft.VisualStudio.TestTools.UITest.Extension.Silverlight

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Coded UI Test enhanced Framework (CUITe) http://cuite.codeplex.com")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CUITe")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011 - 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
