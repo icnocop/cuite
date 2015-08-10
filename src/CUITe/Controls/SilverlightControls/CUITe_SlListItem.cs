e4a019d0db66c73e5d560e1deefb331b49e123af
ankisaxena <ankisaxena@deloitte.com>
2015-08-10 19:40:32 +0530

    Added a new control called 'CUITe_SlListItem'
    This would add the functionality to work with Silverlight list items as
    opposed to only Silverlight list that is supported so far

---
+++ src/CUITe/Controls/SilverlightControls/CUITe_SlListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.SilverlightControls;
+
+namespace CUITe.Controls.SilverlightControls
+{
+    public class CUITe_SlListItem : CUITe_SlControl<SilverlightListItem>
+
+    {
+        public CUITe_SlListItem() : base() { }
+        public CUITe_SlListItem(string searchParameters) : base(searchParameters) { }
+
+    }
+}
