edcf6539e9ff5bbeb23b93875a58876bee1a02b5
ankisaxena <ankisaxena@deloitte.com>
2015-08-13 10:51:07 +0530

    Added SlScrollBar and SlTreeItem controls
    Added support for Silverlight scroll bar and Tree item

---
+++ src/CUITe/Controls/SilverlightControls/CUITe_SlScrollBar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UITesting.SilverlightControls;
+using CUITe.Extensions;
+using CUITe.Controls.HtmlControls;
+using CUITe.Controls.SilverlightControls;
+using Microsoft.VisualStudio.TestTools.UITest.Extension.Silverlight;
+
+namespace CUITe.Extensions.Controls.SilverlightControls
+{
+    public class CUITe_SlScrollBar : CUITe_SlControl<SilverlightControl>
+    {
+        public CUITe_SlScrollBar() : base() { }
+        public CUITe_SlScrollBar(string searchParameters) : base(searchParameters) { }
+    }
+}
---
+++ src/CUITe/Controls/SilverlightControls/CUITe_SlTreeItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting.SilverlightControls;
+using CUITe.Extensions;
+using CUITe.Controls.HtmlControls;
+//using Microsoft.VisualStudio.TestTools.UITest.Extension.
+using CUITe.Controls.SilverlightControls;
+
+namespace CUITe.Extensions.Controls.SilverlightControls
+{
+    /// <summary>
+    /// CUITe wrapper for SilverlightTree.
+    /// </summary>
+    public class CUITe_SlTreeItem : CUITe_SlControl<SilverlightTreeItem>
+    {
+        public CUITe_SlTreeItem() : base() { }
+        public CUITe_SlTreeItem(string searchParameters) : base(searchParameters) { }
+    }
+}
