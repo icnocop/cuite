0845315b6de3d8f423059139f17715602e455338
FantasticFiasco <mattias.kindborg@gmail.com>
2015-08-13 19:49:05 +0200

    Started implementing controls in their own user controls.

---
+++ src/CUITe.ApplicationUnderTest.Wpf/Controls/DocumentViewerControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows.Xps.Packaging;
+
+namespace CUITe.ApplicationUnderTest.Wpf.Controls
+{
+    public partial class DocumentViewerControl
+    {
+        public DocumentViewerControl()
+        {
+            InitializeComponent();
+
+            var document = new XpsDocument(DocumentPath, FileAccess.Read);
+            documentViewer.Document = document.GetFixedDocumentSequence();
+        }
+
+        private static string DocumentPath
+        {
+            get
+            {
+                return Path.Combine(
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                    "Resources",
+                    "Sample.xps");
+            }
+        }
+    }
+}
