60e9f09bb92496b83804eadf3b3dcfdda02deb5f
FantasticFiasco <mattias.kindborg@gmail.com>
2015-08-18 21:02:04 +0200

    Started populating the WinForms AUT with controls.

--- src/Aut.WinForms.Controls/MainForm.Designer.cs
+++ src/Aut.WinForms.Controls/MainForm.Designer.cs
@@ -28,12 +28,313 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.button = new System.Windows.Forms.Button();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label = new System.Windows.Forms.Label();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.radioButton = new System.Windows.Forms.RadioButton();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBoxLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.groupBox.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel.Controls.Add(this.button);
+            this.flowLayoutPanel.Controls.Add(this.checkBox);
+            this.flowLayoutPanel.Controls.Add(this.comboBox);
+            this.flowLayoutPanel.Controls.Add(this.dateTimePicker);
+            this.flowLayoutPanel.Controls.Add(this.groupBox);
+            this.flowLayoutPanel.Controls.Add(this.label);
+            this.flowLayoutPanel.Controls.Add(this.linkLabel);
+            this.flowLayoutPanel.Controls.Add(this.listBox);
+            this.flowLayoutPanel.Controls.Add(this.listView);
+            this.flowLayoutPanel.Controls.Add(this.maskedTextBox);
+            this.flowLayoutPanel.Controls.Add(this.monthCalendar);
+            this.flowLayoutPanel.Controls.Add(this.numericUpDown);
+            this.flowLayoutPanel.Controls.Add(this.pictureBox);
+            this.flowLayoutPanel.Controls.Add(this.progressBar);
+            this.flowLayoutPanel.Controls.Add(this.radioButton);
+            this.flowLayoutPanel.Controls.Add(this.richTextBox);
+            this.flowLayoutPanel.Controls.Add(this.tabControl);
+            this.flowLayoutPanel.Controls.Add(this.textBox);
+            this.flowLayoutPanel.Controls.Add(this.treeView);
+            this.flowLayoutPanel.Controls.Add(this.webBrowser);
+            this.flowLayoutPanel.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(760, 538);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // button
+            // 
+            this.button.AutoSize = true;
+            this.button.Location = new System.Drawing.Point(3, 3);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(89, 23);
+            this.button.TabIndex = 0;
+            this.button.Text = "This is a button";
+            this.button.UseVisualStyleBackColor = true;
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(98, 3);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(118, 17);
+            this.checkBox.TabIndex = 1;
+            this.checkBox.Text = "This is a check box";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(222, 3);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 21);
+            this.comboBox.TabIndex = 2;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(349, 3);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 3;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(639, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(71, 13);
+            this.label.TabIndex = 5;
+            this.label.Text = "This is a label";
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Location = new System.Drawing.Point(3, 51);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(90, 13);
+            this.linkLabel.TabIndex = 6;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "This is a link label";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(99, 54);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(120, 95);
+            this.listBox.TabIndex = 7;
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(225, 54);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(121, 97);
+            this.listView.TabIndex = 8;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // maskedTextBox
+            // 
+            this.maskedTextBox.Location = new System.Drawing.Point(352, 54);
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.Size = new System.Drawing.Size(140, 20);
+            this.maskedTextBox.TabIndex = 9;
+            this.maskedTextBox.Text = "This is a masked text box";
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.Location = new System.Drawing.Point(504, 60);
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.TabIndex = 10;
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(3, 234);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown.TabIndex = 11;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(129, 234);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.TabIndex = 11;
+            this.pictureBox.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(235, 234);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 23);
+            this.progressBar.TabIndex = 12;
+            // 
+            // radioButton
+            // 
+            this.radioButton.AutoSize = true;
+            this.radioButton.Location = new System.Drawing.Point(341, 234);
+            this.radioButton.Name = "radioButton";
+            this.radioButton.Size = new System.Drawing.Size(123, 17);
+            this.radioButton.TabIndex = 13;
+            this.radioButton.TabStop = true;
+            this.radioButton.Text = "This is a radio button";
+            this.radioButton.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(470, 234);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox.TabIndex = 14;
+            this.richTextBox.Text = "This is a rich text box";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(209, 336);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 20);
+            this.textBox.TabIndex = 16;
+            this.textBox.Text = "This is a text box";
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(315, 336);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(121, 97);
+            this.treeView.TabIndex = 17;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(442, 336);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser.TabIndex = 18;
+            // 
+            // groupBox
+            // 
+            this.groupBox.AutoSize = true;
+            this.groupBox.Controls.Add(this.groupBoxLabel);
+            this.groupBox.Location = new System.Drawing.Point(555, 3);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(78, 45);
+            this.groupBox.TabIndex = 4;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Group box";
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(3, 336);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(200, 100);
+            this.tabControl.TabIndex = 15;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxLabel
+            // 
+            this.groupBoxLabel.AutoSize = true;
+            this.groupBoxLabel.Location = new System.Drawing.Point(6, 16);
+            this.groupBoxLabel.Name = "groupBoxLabel";
+            this.groupBoxLabel.Size = new System.Drawing.Size(66, 13);
+            this.groupBoxLabel.TabIndex = 0;
+            this.groupBoxLabel.Text = "This is a text";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "MainForm";
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Application Under Test (WinForms)";
+            this.flowLayoutPanel.ResumeLayout(false);
+            this.flowLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox;
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.RadioButton radioButton;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label groupBoxLabel;
+
     }
 }
 
