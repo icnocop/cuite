0f1b43ac00014b572f2fb301abe8db2daf8188ee
FantasticFiasco <mattias.kindborg@gmail.com>
2015-08-18 20:37:12 +0200

    Added a new empty WinForms Application Under Test project.

---
+++ src/Aut.WinForms.Controls/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Aut.WinForms.Controls
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
