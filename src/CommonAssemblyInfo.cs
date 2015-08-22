1e9767b366a74060e0f551d6d85ee81919da64b2
Rami Abughazaleh <rami.abughazaleh@gmail.com>
2015-08-21 23:35:17 -0700

    updated authors, copyright, and version number to 2.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,14 +3,14 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("Coded UI Test enhanced Framework (CUITe) https://github.com/icnocop/cuite")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("icnocop")]
+[assembly: AssemblyCompany("Suresh Balasubramanian, Matt Gray, Rami Abughazaleh, Mattias Kindborg, and contributors")]
 [assembly: AssemblyProduct("CUITe")]
-[assembly: AssemblyCopyright("Copyright © icnocop 2011 - 2015")]
+[assembly: AssemblyCopyright("Copyright © Suresh Balasubramanian, Matt Gray, Rami Abughazaleh, Mattias Kindborg, and contributors 2011 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyInformationalVersion("1.0.6-beta")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta")]
