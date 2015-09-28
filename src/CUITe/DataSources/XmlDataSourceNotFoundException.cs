378dbea40afdd0888925f0304163126b5157f480
FantasticFiasco <mattias.kindborg@gmail.com>
2015-09-28 19:39:32 +0200

    Renamed 'DataManager' to 'XmlDataSource' and refactored thr class.

---
+++ src/CUITe/DataSources/XmlDataSourceNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace CUITe.DataSources
+{
+    /// <summary>
+    /// Exception thrown by <see cref="XmlDataSource"/> when the specified XML data source isn't
+    /// found.
+    /// </summary>
+    public class XmlDataSourceNotFoundException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlDataSourceNotFoundException"/> class.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <param name="type">The type.</param>
+        /// <param name="fileName">Name of the file.</param>
+        public XmlDataSourceNotFoundException(Assembly assembly, Type type, string fileName)
+            : base(string.Format("Failed to get resource '{0}' from type '{1}' in assembly '{2}'.", fileName, type, assembly.FullName))
+        {
+        }
+    }
+}
---
+++ src/CUITeTest/DataSources/XmlDataSourceTest.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using CUITe.DataSources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CUITeTest.DataSources
+{
+    [TestClass]
+    public class XmlDataSourceTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(XmlDataSourceNotFoundException))]
+        public void InvalidDataSource()
+        {
+            // Act
+            XmlDataSource.GetDataBlock(typeof(XmlDataSourceTest), "InvalidFileName.xml", "indifferent");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataBlockIdNotFoundException))]
+        public void InvalidId()
+        {
+            // Act
+            XmlDataSource.GetDataBlock(typeof(XmlDataSourceTest), "XmlDataSource.xml", "invalidid");
+        }
+
+        [TestMethod]
+        public void NonOverriddenData()
+        {
+            // Act
+            Hashtable data = XmlDataSource.GetDataBlock(typeof(XmlDataSourceTest), "XmlDataSource.xml", "motorcycle");
+
+            // Assert
+            Assert.AreEqual("2", data["numberofwheels"]);
+        }
+
+        [TestMethod]
+        public void OverriddenData()
+        {
+            // Act
+            Hashtable data = XmlDataSource.GetDataBlock(typeof(XmlDataSourceTest), "XmlDataSource.xml", "motorcyclewithsidecar");
+
+            // Assert
+            Assert.AreEqual("3", data["numberofwheels"]);
+        }
+
+        [TestMethod]
+        public void FirstGenerationData()
+        {
+            // Act
+            Hashtable data = XmlDataSource.GetDataBlock(typeof(XmlDataSourceTest), "XmlDataSource.xml", "name");
+
+            // Assert
+            AssertName(data);
+        }
+
+        [TestMethod]
+        public void SecondGenerationData()
+        {
+            // Act
+            Hashtable data = XmlDataSource.GetDataBlock(typeof(XmlDataSourceTest), "XmlDataSource.xml", "dateofbirth");
+
+            // Assert
+            AssertDateOfBirth(data);
+        }
+
+        [TestMethod]
+        public void ThirdGenerationData()
+        {
+            // Act
+            Hashtable data = XmlDataSource.GetDataBlock(typeof(XmlDataSourceTest), "XmlDataSource.xml", "currentaddress");
+
+            // Assert
+            AssertCurrentAddress(data);
+        }
+
+        private static void AssertName(Hashtable data)
+        {
+            Assert.AreEqual("Suresh", data["firstname"]);
+            Assert.AreEqual("Balasubramanian", data["lastname"]);
+        }
+
+        private static void AssertDateOfBirth(Hashtable data)
+        {
+            AssertName(data);
+
+            Assert.AreEqual("04/19/1973", data["date"]);
+            Assert.AreEqual("38", data["age"]);
+        }
+
+        private void AssertCurrentAddress(Hashtable data)
+        {
+            AssertName(data);
+            AssertDateOfBirth(data);
+
+            Assert.AreEqual("Kondapur, Hyderabad", data["address"]);
+            Assert.AreEqual("Indian", data["nationality"]);
+        }
+    }
+}
