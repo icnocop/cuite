857a01d9be6ddc5b068df1f1b67e688115a3b663
FantasticFiasco <mattias.kindborg@gmail.com>
2015-10-01 22:57:09 +0200

    Added tests for 'Sut.Wpf.ScreenComponents'.

---
+++ src/Sut.Wpf.ScreenComponentsTest/Mappings/LowerRightComponent.cs
@@ -0,0 +1,14 @@
+﻿using CUITe.Controls.WpfControls;
+using CUITe.Mappings;
+using CUITe.SearchConfigurations;
+
+namespace Sut.Wpf.ScreenComponentsTest.Mappings
+{
+    public class LowerRightComponent : ScreenComponent
+    {
+        public bool RadioButtonExists
+        {
+            get { return Find<WpfRadioButton>(By.AutomationId("oEOhLBF5ske3yGfbHhThuA")).Exists; }
+        }
+    }
+}
