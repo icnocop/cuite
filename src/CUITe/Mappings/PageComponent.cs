539d9dc4bcb25ed9225c8d0127bcc5310a412a37
FantasticFiasco <mattias.kindborg@gmail.com>
2015-10-02 14:59:08 +0200

    Implemented a class called 'PageComponent' that takes the responsibility of 'ScreenComponent' when it comes to HTML applications.

---
+++ src/CUITe/Mappings/PageComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CUITe.Controls;
+using CUITe.SearchConfigurations;
+using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace CUITe.Mappings
+{
+    /// <summary>
+    /// Abstract class representing a page component in a HTML application.
+    /// </summary>
+    /// <remarks>
+    /// A <see cref="Page"/> with a overwhelming number of controls can be split into logical
+    /// components, thus providing better test code maintainability.
+    /// </remarks>
+    public abstract class PageComponent
+    {
+        private UITestControl searchLimitContainer;
+
+        /// <summary>
+        /// Gets or sets the search limit container.
+        /// </summary>
+        internal UITestControl SearchLimitContainer
+        {
+            get { return searchLimitContainer; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                searchLimitContainer = value;
+            }
+        }
+
+        /// <summary>
+        /// Finds the control object from the descendants of this control using the specified
+        /// search configuration.
+        /// </summary>
+        /// <typeparam name="T">The type of control to find.</typeparam>
+        /// <param name="searchConfiguration">The search configuration.</param>
+        /// <exception cref="InvalidSearchPropertyNamesException">
+        /// Search configuration contains a property namely that isn't applicable on the control.
+        /// </exception>
+        protected T Find<T>(By searchConfiguration = null) where T : ControlBase
+        {
+            if (searchLimitContainer == null)
+                throw new InvalidOperationException("The page component has not been configured with a search limit container.");
+
+            return searchLimitContainer.Find<T>(searchConfiguration);
+        }
+    }
+}
---
+++ src/Sut.Html.PageComponentsTest/Mappings/LowerLeftComponent.cs
@@ -0,0 +1,14 @@
+﻿using CUITe.Controls.HtmlControls;
+using CUITe.Mappings;
+using CUITe.SearchConfigurations;
+
+namespace Sut.Html.PageComponentsTest.Mappings
+{
+    public class LowerLeftComponent : PageComponent
+    {
+        public bool RadioButtonExists
+        {
+            get { return Find<HtmlRadioButton>(By.Id("lowerleft")).Exists; }
+        }
+    }
+}
