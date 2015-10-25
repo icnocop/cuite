04f307fc1bc2c4e74ba989cc06c774812032b16f
FantasticFiasco <mattias.kindborg@gmail.com>
2015-10-25 22:57:03 +0100

    Added a 'System Under Test' that will demonstrate the workflow feature.

---
+++ src/Sut.Wpf.Workflows/Pages/NamePageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Sut.Wpf.Workflows.Pages
+{
+    public class NamePageViewModel : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string surname;
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName == value)
+                    return;
+
+                firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (surname == value)
+                    return;
+
+                surname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ src/Sut.Wpf.Workflows/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Sut.Wpf.Workflows
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute)
+            : this(execute, () => true)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            if (canExecute == null)
+                throw new ArgumentNullException("canExecute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
