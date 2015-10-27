5b69d459e6e5446eb63f3f64367b8ad2620b8af7
FantasticFiasco <mattias.kindborg@gmail.com>
2015-10-27 20:36:17 +0100

    Added workflow tests for HTML.

---
+++ src/Sut.Html.WorkflowsTest/ObjectRepository/FinishedPage.cs
@@ -0,0 +1,14 @@
+﻿using CUITe.Controls.HtmlControls;
+using CUITe.ObjectRepository;
+using CUITe.SearchConfigurations;
+
+namespace Sut.Html.WorkflowsTest.ObjectRepository
+{
+    public class FinishedPage : Page
+    {
+        public bool CongratulationsExists
+        {
+            get { return Find<HtmlParagraph>(By.Id("congratulations")).Exists; }
+        }
+    }
+}
---
+++ src/Sut.Html.WorkflowsTest/WorkflowsTest.cs
@@ -0,0 +1,96 @@
+﻿using System.IO;
+using CUITe.ObjectRepository;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sut.Html.WorkflowsTest.ObjectRepository;
+using Sut.Html.WorkflowsTest.Workflows;
+using TestHelpers;
+
+namespace Sut.Html.WorkflowsTest
+{
+    /// <summary>
+    /// Summary description for WorkflowsTest
+    /// </summary>
+    [CodedUITest]
+    public class WorkflowsTest
+    {
+        /// <summary>
+        /// Gets or sets the test context which provides information about and functionality for
+        /// the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void StepThroughWizard()
+        {
+            using (var finishedWebPage = new TempWebPage(Finished))
+            using (var addressWebPage = new TempWebPage(string.Format(Address, Path.GetFileName(finishedWebPage.FilePath))))
+            using (var nameWebPage = new TempWebPage(string.Format(Name, Path.GetFileName(addressWebPage.FilePath))))
+            {
+                // Arrange
+                var namePage = Page.Launch<NamePage>(nameWebPage.FilePath);
+                var workflow = new WizardWorkflow(namePage);
+
+                // Act
+                FinishedPage finishedPage = workflow.StepThrough();
+
+                // Assert
+                Assert.IsTrue(finishedPage.CongratulationsExists);
+            }
+        }
+
+        #region Helper properties
+
+        private static string Name
+        {
+            get
+            {
+                return
+                    @"<html>
+                        <body>
+                          <div>
+                            <p>First name: <input type=""text"" id=""firstname"" /></p>
+                            <p>Surname: <input type=""text"" id=""surname"" /></p>
+                          </div>
+                          <button id=""next"" onclick=""location.href='{0}';"">Next</button>
+                        </body>
+                      </html>";
+            }
+        }
+
+        private static string Address
+        {
+            get
+            {
+                return
+                    @"<html>
+                        <body>
+                          <div>
+                            <p>Address: <input type=""text"" id=""address"" /></p>
+                            <p>City: <input type=""text"" id=""city"" /></p>
+                            <p>Postal Code: <input type=""text"" id=""postalcode"" /></p>
+                            <p>State: <input type=""text"" id=""state"" /></p>
+                          </div>
+                          <button id=""next"" onclick=""location.href='{0}';"">Next</button>
+                        </body>
+                      </html>";
+            }
+        }
+
+        private static string Finished
+        {
+            get
+            {
+                return
+                    @"<html>
+                        <body>
+                          <p id=""congratulations"">Congratulations!!!</p>
+                          <p>You made it through this worthless wizard.</p>
+                        </body>
+                      </html>";
+            }
+        }
+
+        #endregion
+    }
+}
