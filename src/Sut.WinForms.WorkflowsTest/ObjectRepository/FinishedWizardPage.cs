bbf819ce90099e7e053e8ee294d8f72f4acec217
FantasticFiasco <mattias.kindborg@gmail.com>
2015-10-29 22:34:49 +0100

    Added test for workflows in WinForms. The test is currently disabled, same as other WinForms tests that cannot access a control using its name property. Is something I will have to fix later.

---
+++ src/Sut.WinForms.WorkflowsTest/ObjectRepository/FinishedWizardPage.cs
@@ -0,0 +1,14 @@
+﻿using CUITe.Controls.WinControls;
+using CUITe.ObjectRepository;
+using CUITe.SearchConfigurations;
+
+namespace Sut.WinForms.WorkflowsTest.ObjectRepository
+{
+    public class FinishedWizardPage : Screen
+    {
+        public bool CongratulationsExists
+        {
+            get { return Find<WinText>(By.Name("labelCongratulations")).Exists; }
+        }
+    }
+}
