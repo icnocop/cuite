a9f96975cd5ccd2e9d14affaf972f39729a39640
Rami Abughazaleh <rami.abughazaleh@gmail.com>
2015-12-02 01:44:12 -0800

    explicitly setting low integrity level on the output directory to fix appveyor CI build

---
+++ src/Sut.HtmlTest/ProcessRunner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace Sut.HtmlTest
+{
+    using System.Diagnostics;
+    using System.Threading;
+
+    internal static class ProcessRunner
+    {
+        internal static RunResult Run(
+           string fileName,
+           string arguments)
+        {
+            RunResult runResult = new RunResult();
+
+            using (Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo(fileName, arguments)
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    FileName = fileName,
+                    Arguments = arguments
+                }
+            })
+            {
+                StringBuilder output = new StringBuilder();
+                StringBuilder error = new StringBuilder();
+
+                Action<string, string, AutoResetEvent, StringBuilder> dataReceivedHandler = (prefix, data, autoResetEvent, stringBuilder) =>
+                {
+                    if (data == null)
+                    {
+                        autoResetEvent.Set();
+                    }
+                    else
+                    {
+                        string message = string.Format("[{0}] {1}: {2}", DateTime.Now, prefix, data);
+                        Trace.WriteLine(message);
+                        stringBuilder.AppendLine(message);
+                    }
+                };
+
+                using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+                using (AutoResetEvent errorWaitHandle = new AutoResetEvent(false))
+                {
+                    process.OutputDataReceived += (sender, e) => dataReceivedHandler("OUT", e.Data, outputWaitHandle, output);
+                    process.ErrorDataReceived += (sender, e) => dataReceivedHandler("ERR", e.Data, errorWaitHandle, error);
+
+                    Trace.WriteLine(string.Format("\"{0}\" {1}", fileName, arguments));
+
+                    process.Start();
+
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+
+                    process.WaitForExit();
+
+                    // process completed
+                    runResult.ExitCode = process.ExitCode;
+                    runResult.StandardOutput = output.ToString();
+                    runResult.StandardError = error.ToString();
+                }
+            }
+
+            return runResult;
+        }
+    }
+}
---
+++ src/Sut.HtmlTest/RunResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Sut.HtmlTest
+{
+    internal class RunResult
+    {
+        internal int ExitCode { get; set; }
+        internal string StandardOutput { get; set; }
+        internal string StandardError { get; set; }
+    }
+}
