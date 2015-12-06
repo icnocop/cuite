d72b212252a30ffb59785d859989919c96972056
Rami Abughazaleh <icnocop@users.noreply.github.com>
2015-12-06 01:47:51 -0800

    renamed CUITe_SlListItem to SilverlightListItem and added it to SilverlightControls

---
+++ src/CUITe.Silverlight/Controls/SilverlightControls/SilverlightListItem.cs
@@ -0,0 +1,30 @@
+﻿#if SILVERLIGHT_SUPPORT
+using CUITControls = Microsoft.VisualStudio.TestTools.UITesting.SilverlightControls;
+
+namespace CUITe.Controls.SilverlightControls
+{
+    using CUITe.SearchConfigurations;
+
+    public class SilverlightListItem : SilverlightControl<CUITControls.SilverlightListItem>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SilverlightListItem"/> class.
+        /// </summary>
+        /// <param name="searchConfiguration">The search configuration.</param>
+        public SilverlightListItem(By searchConfiguration = null)
+            : this(new CUITControls.SilverlightListItem(), searchConfiguration)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SilverlightListItem"/> class.
+        /// </summary>
+        /// <param name="sourceControl">The source control.</param>
+        /// <param name="searchConfiguration">The search configuration.</param>
+        public SilverlightListItem(CUITControls.SilverlightListItem sourceControl, By searchConfiguration = null)
+            : base(sourceControl, searchConfiguration)
+        {
+        }
+    }
+}
+#endif
