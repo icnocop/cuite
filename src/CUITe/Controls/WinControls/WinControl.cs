37628bf59f5742370dfc74e48b7ce041a91e6f9d
FantasticFiasco <mattias.kindborg@gmail.com>
2016-03-14 22:26:03 +0100

    Created a non-abstract test control for WPF, WinForms and Silverlight.

--- src/CUITe/Controls/WinControls/WinControl.cs
+++ src/CUITe/Controls/WinControls/WinControl.cs
@@ -1,133 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using CUITe.SearchConfigurations;
-using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using CUITe.SearchConfigurations;
 using CUITControls = Microsoft.VisualStudio.TestTools.UITesting.WinControls;
 
 namespace CUITe.Controls.WinControls
 {
     /// <summary>
-    /// Base class for all test controls in the user interface (UI) of Windows Forms.
+    /// Represents a control to test the user interface (UI) of Windows Forms.
     /// </summary>
-    /// <typeparam name="T">The source control type.</typeparam>
-    public abstract class WinControl<T> : ControlBase<T> where T : CUITControls.WinControl
+    public class WinControl : WinControl<CUITControls.WinControl>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="WinControl{T}"/> class.
+        /// Initializes a new instance of the <see cref="WinControl"/> class.
+        /// </summary>
+        /// <param name="searchConfiguration">The search configuration.</param>
+        public WinControl(By searchConfiguration = null)
+            : this(new CUITControls.WinControl(), searchConfiguration)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WinControl"/> class.
         /// </summary>
         /// <param name="sourceControl">The source control.</param>
         /// <param name="searchConfiguration">The search configuration.</param>
-        protected WinControl(T sourceControl, By searchConfiguration = null)
+        public WinControl(CUITControls.WinControl sourceControl, By searchConfiguration = null)
             : base(sourceControl, searchConfiguration)
         {
-        }
-
-        /// <summary>
-        /// Gets the parent of the control.
-        /// </summary>
-        /// <exception cref="InvalidTraversalException">
-        /// Error occurred when traversing the control tree.
-        /// </exception>
-        public override ControlBase Parent
-        {
-            get
-            {
-                WaitForControlReadyIfNecessary();
-
-                try
-                {
-                    return ControlBaseFactory.Create(SourceControl.GetParent());
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-                    throw new InvalidTraversalException(string.Format("({0}).Parent", SourceControl.GetType().Name));
-                }
-            }
-        }
-
-        /// <summary>
-        /// Gets the previous sibling of the control.
-        /// </summary>
-        /// <exception cref="InvalidTraversalException">
-        /// Error occurred when traversing the control tree.
-        /// </exception>
-        public override ControlBase PreviousSibling
-        {
-            get { return null; }
-        }
-
-        /// <summary>
-        /// Gets the next sibling of the control.
-        /// </summary>
-        /// <exception cref="InvalidTraversalException">
-        /// Error occurred when traversing the control tree.
-        /// </exception>
-        public override ControlBase NextSibling
-        {
-            get { return null; }
-        }
-
-        /// <summary>
-        /// Gets the first child of the control.
-        /// </summary>
-        /// <exception cref="InvalidTraversalException">
-        /// Error occurred when traversing the control tree.
-        /// </exception>
-        public override ControlBase FirstChild
-        {
-            get { return null; }
-        }
-
-        /// <summary>
-        /// Returns a sequence of all first level children of the control.
-        /// </summary>
-        public override IEnumerable<ControlBase> GetChildren()
-        {
-            return Enumerable.Empty<ControlBase>();
-        }
-
-        /// <summary>
-        /// Adds all search property in the provided collection.
-        /// </summary>
-        /// <param name="searchProperties">The search properties.</param>
-        internal override void AddSearchProperties(PropertyExpressionCollection searchProperties)
-        {
-            if (searchProperties == null)
-                throw new ArgumentNullException("searchProperties");
-
-            MoveControlNames(searchProperties);
-            
-            base.AddSearchProperties(searchProperties);
-        }
-
-        /// <summary>
-        /// Moves the property expressions with property name
-        /// <see cref="CUITControls.WinControl.PropertyNames.ControlName"/>. from the control to
-        /// search for to its parent.
-        /// </summary>
-        /// <remarks>
-        /// Searching for WinForms controls differs slightly from searching for WPF controls.
-        /// When searching for WPF controls using e.g. their automation id, the automation id
-        /// search property is added to the WPF control.
-        /// When searching for WinForms controls using their control name, the control name search
-        /// property is added to the parent WinForms control, not the actual control itself.
-        /// </remarks>
-        private void MoveControlNames(PropertyExpressionCollection searchProperties)
-        {
-            PropertyExpression[] controlNameExpressions = searchProperties
-                .Where(searchProperty => searchProperty.PropertyName == CUITControls.WinControl.PropertyNames.ControlName)
-                .ToArray();
-
-            if (!controlNameExpressions.Any())
-                return;
-
-            foreach (PropertyExpression controlNameExpression in controlNameExpressions)
-            {
-                searchProperties.Remove(controlNameExpression);
-            }
-
-            SourceControl.GetParent().SearchProperties.AddRange(controlNameExpressions);
         }
     }
 }
